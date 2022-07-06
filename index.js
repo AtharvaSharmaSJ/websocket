51935e4da125082460d7000c1b124727d9be8260
AtharvaSharmaSJ <98635816+AtharvaSharmaSJ@users.noreply.github.com>
2022-07-06 17:32:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Server } from '@hocuspocus/server'
 import { RocksDB } from '@hocuspocus/extension-rocksdb'
 
 const server = Server.configure({
-  port: 80,
+  port:  process.env.PORT || 8080,
   extensions: [
     new RocksDB({ path: './database' }),
   ],
